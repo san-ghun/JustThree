3d32a1cf8086f31aa7002476738cb62c4efbedef
Sanghun <po4865@gmail.com>
2022-09-23 22:25:46 +0200

    init: Initiate project with default UIKit storyboard

---
+++ JustThree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustThree
+//
+//  Created by Sanghun Park on 23.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
