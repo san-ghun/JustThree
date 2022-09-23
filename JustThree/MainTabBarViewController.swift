3c705843205cfdef2ce26d1cf47bd48f51cf737b
Sanghun <po4865@gmail.com>
2022-09-23 23:48:59 +0200

    feat: Set up programmatic UI
    - Delete `Main.storyboard` related settings to prepare to set up Programmatic UI
    - Modify `SceneDelegate.swift` to set up root view controller.
    
    MainTabBarViewController.swift
    - Add new file with UITabBarController to configure tab bar.
    - Add four tab bar items in the tab bar with UIImage and title.
    - Configure each view controllers to UINavigationController with UICollectionViewController that initiate with UICollectionViewCompositionalLayout as collectionViewLayout.
    - Add new function `listLayout()` that configure and return UICollectionViewCompositionalLayout
    
    ReminderListViewController.swift
    - Add new file with UICollectionViewController to displays reminders in a list.
    
    SettingListViewController.swift
    - Add new file with UICollectionViewController to displays setting items in a list.

---
+++ JustThree/MainTabBarViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MainTabBarViewController.swift
+//  JustThree
+//
+//  Created by Sanghun Park on 23.09.22.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemYellow
+        let listLayout = listLayout()
+        
+        let vc1 = UINavigationController(rootViewController: ReminderListViewController(collectionViewLayout: listLayout))
+        let vc2 = UINavigationController(rootViewController: ReminderListViewController(collectionViewLayout: listLayout))
+        let vc3 = UINavigationController(rootViewController: ReminderListViewController(collectionViewLayout: listLayout))
+        let vc4 = UINavigationController(rootViewController: SettingListViewController(collectionViewLayout: listLayout))
+        
+        vc1.tabBarItem.image = UIImage(systemName: "die.face.1.fill")
+        vc2.tabBarItem.image = UIImage(systemName: "die.face.2.fill")
+        vc3.tabBarItem.image = UIImage(systemName: "die.face.3.fill")
+        vc4.tabBarItem.image = UIImage(systemName: "gearshape.fill")
+        
+        vc1.title = "List 1"
+        vc2.title = "List 2"
+        vc3.title = "List 3"
+        vc4.title = "Setting"
+        
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+    }
+
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        
+        // Create a section in a list layout
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+
+}
+
