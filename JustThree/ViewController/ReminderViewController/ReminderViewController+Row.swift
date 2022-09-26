8089144b67affae520201a686243416518686ff5
Sanghun <po4865@gmail.com>
2022-09-26 16:00:33 +0200

    feat: Create an enumeration for rows
    ReminderViewController+Row.swift
    - Create an enumeration `Row` to describe the detail rows and to provide the text for each row.
    
    ReminderViewController.swift
    - Create a function `text(for:)` that returns the text associated with the given row.

---
+++ JustThree/ViewController/ReminderViewController/ReminderViewController+Row.swift
@@ -0,0 +1,39 @@
+//
+//  ReminderViewController+Row.swift
+//  JustThree
+//
+//  Created by Sanghun Park on 26.09.22.
+//
+
+import UIKit
+
+extension ReminderViewController {
+    enum Row: Hashable {
+        case viewDate
+        case viewNotes
+        case viewTime
+        case viewTitle
+        
+        var imageName: String? {
+            switch self {
+            case .viewDate: return "calendar.circle"
+            case .viewNotes: return "square.and.pencil"
+            case .viewTime: return "clock"
+            default: return nil
+            }
+        }
+        
+        var image: UIImage? {
+            guard let imageName = imageName else { return nil }
+            let configuration = UIImage.SymbolConfiguration(textStyle: .headline)
+            return UIImage(systemName: imageName, withConfiguration: configuration)
+        }
+        
+        var textStyle: UIFont.TextStyle {
+            switch self {
+            case .viewTitle: return .headline
+            default: return .subheadline
+            }
+        }
+    }
+}
