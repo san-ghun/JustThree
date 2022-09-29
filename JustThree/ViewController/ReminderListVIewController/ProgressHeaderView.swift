8980167b8d488c14f1602925c79951f7aaa58b7a
Sanghun <po4865@gmail.com>
2022-09-29 22:42:55 +0200

    feat: Create relatioinal constraints and customize the appearance of the progress circle
    ProgressHeaderView.swift
    - Create a new class `ProgressHeaderView` that inherits from UICollectionReusableView.
    - Add a static property `elementKind` to specifies a type of supplementary view that the collection view can present, in this case as a header view.
    - Create a property `progress`  of type CGFloat with a default value of 0.
    - Add private properties to create containerView and progressView subviews, initializing with zero-sized frame.
    - Override initializer with calling `super.init`, and call `prepareSubviews().
    - Override `layoutSubviews()`, and call the superview's implementation,`super.layoutSubviews()`.
    	- Enable `masksToBounds` in the container view's layer, and adjust the corner radius.
    - Create a function `prepareSubviews()` to add the subviews to the view hierarchy.
    	- Add progressView as subview of containerView, and add containerView as a subview of the progress header view.
    	- Disable `translatesAutoresizingMaskIntoConstraints` for the subviews.
    	- Maintain a 1:1 fixed aspect ratio for the superview and containerView.
    	- Center the containerView and progressView horizontally and vertically in the layout frame.
    	- Scale the containerView to 85 percent of the size of its superview.
    	- Vertically constrain the progressView by setting the top anchor, bottom anchor, leading anchor, and trailing anchor to the anchors of the containerView.
    	- Assign background colors to .clear.

---
+++ JustThree/ViewController/ReminderListVIewController/ProgressHeaderView.swift
@@ -0,0 +1,61 @@
+//
+//  ProgressHeaderView.swift
+//  JustThree
+//
+//  Created by Sanghun Park on 29.09.22.
+//
+
+import UIKit
+
+class ProgressHeaderView: UICollectionReusableView {
+    static var elementKind: String { UICollectionView.elementKindSectionHeader }
+    
+    var progress: CGFloat = 0
+    
+    private let containerView = UIView(frame: .zero)
+    private let progressView = UIView(frame: .zero)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        prepareSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        containerView.layer.masksToBounds = true
+        containerView.layer.cornerRadius = 0.5 * containerView.bounds.width
+    }
+    
+    private func prepareSubviews() {
+        containerView.addSubview(progressView)
+        addSubview(containerView)
+        
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            
+            heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1),
+            
+            containerView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.85),
+            containerView.heightAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 1),
+            containerView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            containerView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            progressView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            progressView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            progressView.topAnchor.constraint(equalTo: topAnchor),
+            progressView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            progressView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+        
+        backgroundColor = .clear
+        containerView.backgroundColor = .clear
+    }
+}
